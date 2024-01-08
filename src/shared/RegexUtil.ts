54c533bbce64c0ae64c22780caa2843d9ff121b0
Rinku <shark4rkcoding@gmail.com>
2024-01-08 10:15:18 +0800

    fix: Replace password regex pattern with correct one

--- src/shared/RegexUtil.ts
+++ src/shared/RegexUtil.ts
@@ -1,4 +1,4 @@
 export const RegexUtil = {
   emailReg: /\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/,
-  passwordReg: /\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/,
+  passwordReg: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\!\@\#\$\%\^\&\*]).{8,20}$/
 };

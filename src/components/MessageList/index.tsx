5fe3470212c61c751e23d4ef0f4f38772aa167b3
Rickh <hpx210650@gmail.com>
2024-01-13 23:20:08 +0800

    ✨ feat: message list style

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { MessageListProps } from './message-list-props.model';
 import { Message } from '../Message';
 
 export function MessageList({messageList, responseProgress}: MessageListProps) {
+  const [hasInit, setHasInit] = React.useState<boolean>(false);
+
+  const messagesEndRef = useRef(null);
+
+  useEffect(() => {
+    // 滾到最下面
+    (messagesEndRef.current as any)?.scrollIntoView({ behavior: "smooth" });
+
+    // 已經執行過一次
+    if(!hasInit) {
+      setHasInit(true);
+    }
+
+  }, [messageList, responseProgress]);
+
+  const handleScroll = (evt) => {
+    if(evt.currentTarget.scrollTop <= 30) {
+      loadMore();
+    }
+  }
+
+  const loadMore = () => {
+    console.log('loadMore');
+  };
+
   return (
     <div
       style={{
         flex: 1,
-        paddingBottom: '2rem'
+        paddingBottom: '2rem',
+        height: 'calc(100vh - 250px)',
+        width: '100%',
+        overflow: 'auto'
       }}
+      onScroll={handleScroll}
     >
       {
         messageList.map(message => (
           <Message {...message}/>
         ))
       }
-      
       {
         responseProgress &&
         <Message
@@ -25,8 +53,7 @@ export function MessageList({messageList, responseProgress}: MessageListProps) {
           progress={responseProgress}
         />
       }
-      
-      
+      <div ref={messagesEndRef} />
     </div>
   );
 }

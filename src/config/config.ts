7c2beb45770974251dfcd17f519d67bedb073628
Rickh <hpx210650@gmail.com>
2024-01-28 13:25:02 +0800

    🐞 fix:

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,5 @@
 export const CONFIG = {
   tokenKey: '_token',
   authKey: '_auth',
-  // apiUrl: 'http://127.0.0.1:8000/api/v1',
-  apiUrl: 'http://192.168.1.103:8000/api/v1',
+  apiUrl: 'http://127.0.0.1:8000/api/v1',
 };

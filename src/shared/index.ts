17d94d8568f052d4fc3770bccb6d66c433556cdd
Rickh <hpx210650@gmail.com>
2024-02-17 15:57:07 +0800

    🦄 refactor: remove mui related file

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,7 +1,7 @@
-export * from './RoutesGuard';
-export * from './GuestGuard';
-export * from './http';
-export * from './InjectAxiosInterceptors';
-export * from './Snackbar';
-export * from './RegexUtil';
-export * from './shared-theme';
+export * from "./RoutesGuard";
+export * from "./GuestGuard";
+export * from "./http";
+export * from "./InjectAxiosInterceptors";
+export * from "./Snackbar";
+export * from "./RegexUtil";
+// export * from "./shared-theme";

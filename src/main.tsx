8b711e17f02db40554696c4a11f11c2f761f35eb
Rickh <hpx210650@gmail.com>
2024-02-17 15:05:14 +0800

    🦄 refactor: remove mui package

--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'
-import { InjectAxiosInterceptors, sharedTheme } from './shared'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { InjectAxiosInterceptors } from "./shared";
 
-import {
-  SnackbarUtilsConfigurator,
-} from './shared';
+import { SnackbarUtilsConfigurator } from "./shared";
 
-import { SnackbarProvider } from 'notistack';
-import { ThemeProvider } from '@mui/material/styles'
+import { SnackbarProvider } from "notistack";
 
 // eslint-disable-next-line react/no-deprecated
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={sharedTheme}>
-      <SnackbarProvider
-        maxSnack={3}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'center'
-        }}
-        autoHideDuration={1000}
-      >
-        <SnackbarUtilsConfigurator />
-        <BrowserRouter>
-          <InjectAxiosInterceptors />
-          <App />
-        </BrowserRouter>
-      </SnackbarProvider>
-    </ThemeProvider>
+    <SnackbarProvider
+      maxSnack={3}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "center",
+      }}
+      autoHideDuration={1000}
+    >
+      <SnackbarUtilsConfigurator />
+      <BrowserRouter>
+        <InjectAxiosInterceptors />
+        <App />
+      </BrowserRouter>
+    </SnackbarProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
